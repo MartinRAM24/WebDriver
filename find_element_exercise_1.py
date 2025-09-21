a3ead636a208c274ed218ac6ce281ec968cf38e2
Martin Ricardo Avalos Mendoza <martinquetza@gmail.com>
2025-09-21 14:56:45 -0600

    continuacion de practicas

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -1,12 +1,17 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
+time.sleep(5)
+
 # Buscar el título
-driver...
+title_element = driver.find_element(By.CSS_SELECTOR, ".auth-form__title")
+
+print("Login form title:", title_element.text)
 
 # Cerrar el navegador
-driver...
+driver.quit()
--- get_text.py
+++ get_text.py
@@ -3,22 +3,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "email")))
 
 # Buscar el campo Correo electrónico y rellenarlo
-...
+WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.ID, "email"))).send_keys("ketzaypayola@gmail.com")
 
 # Buscar el campo Contraseña y rellenarlo
-...
+WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.ID, "password"))).send_keys("Mar241198")
 
 # Buscar el botón Iniciar sesión y hacer clic en él
-...
+WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, ".auth-form__button"))).click()
 
 # Agregar una espera explícita para que se cargue la página
-WebDriverWait(...).until(...)
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".header__user")))
 
 # Buscar el botón, recuperar su texto y comprobar que el valor del texto es 'Cerrar sesión'
-assert ...
+CS = WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "header__logout"))).text
+assert CS == "Cerrar sesión", f"Se esperaba 'Cerrar sesión', pero se obtuvo '{CS}'"
 
 driver.quit()
